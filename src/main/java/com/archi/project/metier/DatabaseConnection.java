f40ec1534286e505671d8a5b920adca449ee9e7a
HippManIng02 <yao-hippolyte.folly@ens.uvsq.fr>
2024-11-17 22:17:49 +0100

    Modification de la fonction de connexion à la base de donnée

--- src/main/java/com/archi/project/metier/DatabaseConnection.java
+++ src/main/java/com/archi/project/metier/DatabaseConnection.java
@@ -1,7 +1,7 @@
 package com.archi.project.metier;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -11,27 +11,30 @@ public class DatabaseConnection {
     private static String url;
     private static String user;
     private static String password;
+    
+    private static final String ENV_CONFIG_PATH = "DB_CONFIG_PATH"; // Nom de la variable d'environnement
 
     private static Connection connection;
 
     // Chargement des propriétés avec une block static (l'idée est d'exécuté une seule fois ce block lors d'une premier chargement par le JVM)
     static {
-        try (InputStream input = DatabaseConnection.class.getClassLoader().getResourceAsStream("database.properties")) {
-            Properties prop = new Properties();
-            if (input == null) {
-                System.out.println("Désolé, le fichier database.properties est introuvable.");
-            }
-            //Chargement des clé-valeurs depuis le fichier database.properties dans l'objet prop
-            prop.load(input);
+    	Properties prop = new Properties();
+        String configPath = System.getenv(ENV_CONFIG_PATH); // Récupérer le chemin depuis la variable d'environnement
 
-            // Lecture des propriétés de connection
-            url = prop.getProperty("db.url");
-            user = prop.getProperty("db.user");
-            password = prop.getProperty("db.password");
-
-        } catch (IOException ex) {
-            ex.printStackTrace();
+        if (configPath == null || configPath.isEmpty()) {
+            System.err.println("Erreur : La variable d'environnement 'DB_CONFIG_PATH' n'est pas définie.");
         }
+
+        try (FileInputStream fis = new FileInputStream(configPath)) {
+            // Charger les propriétés du fichier
+            prop.load(fis);
+        } catch (IOException e) {
+            System.err.println("Erreur lors du chargement du fichier de configuration : " + e.getMessage());
+        }
+        
+        url = prop.getProperty("db.url");
+        user = prop.getProperty("db.user");
+        password = prop.getProperty("db.password");
     }
     /*
      * Méthode static permettant d'assurer l'utilisation d'une seule connection dans toute l'application 
@@ -42,4 +45,5 @@ public class DatabaseConnection {
         }
         return connection;
     }
+    
 }
